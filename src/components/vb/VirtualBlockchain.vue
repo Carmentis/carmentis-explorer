3d3928c562fce311dacb05d13e22c75e389ccfea
Gaël Marcadet <gamarcadet@gmail.com>
2026-03-05 15:02:38 +0100

    Add Virtual Blockchain details view
    - Create `VirtualBlockchain.vue` to display details of a virtual blockchain.
    - Use `useRoute` for retrieving blockchain ID and `useBlockchainStore` to load blockchain details.
    - Enhance UI with a title header.

--- src/components/vb/VirtualBlockchain.vue
+++ src/components/vb/VirtualBlockchain.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { Hash, type VirtualBlockchain } from '@cmts-dev/carmentis-sdk/client'
+import { useBlockchainStore } from '@/stores/blockchain.ts'
+
+const route = useRoute()
+const vbId = ref(route.params.vbId)
+const vb = ref<VirtualBlockchain | null>(null)
+const blockchainStore = useBlockchainStore()
+onMounted(async () => {
+    const provider = blockchainStore.getProvider
+    vb.value = await provider.loadVirtualBlockchain(Hash.fromHex(vbId.value))
+})
+</script>
+
 <template>
-    Vb
+    <h3>Virtual Blockchain Details</h3>
 </template>
